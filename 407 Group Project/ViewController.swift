c690e73a631ecdb4cc6c15c7f50b671b86553629
ZacharyHirn <Cressida@cressida-mac.local>
2015-11-27 13:55:37 -0600

    Initial Commit

---
+++ 407 Group Project/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  407 Group Project
+//
+//  Created by Zachary Hirn on 11/27/15.
+//  Copyright © 2015 Zachary Hirn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
