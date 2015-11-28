f8c4243ab8486dde2235a78dbea87888921924ab
ZacharyHirn <Cressida@Cressida-Mac.local>
2015-11-27 18:12:22 -0600

    Added Basic movement
    I have included code that moves a simple square across the screen when
    you click on the screen and also when you drag on the screen.

--- 407 Group Project/ViewController.swift
+++ 407 Group Project/ViewController.swift
@@ -9,9 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var location = CGPoint(x: 0, y: 0)
+    
+    @IBOutlet weak var Person: UIImageView!
 
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        //var touch : UITouch! = touches.anyObject() as UITouch
+        
+        let touch = touches.first! as UITouch
+        
+        location = touch.locationInView(self.view)
+        
+        Person.center = location
+    }
+    
+    override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        //var touch : UITouch! = touches.anyObject() as UITouch
+        
+        let touch = touches.first! as UITouch
+        
+        location = touch.locationInView(self.view)
+        
+        Person.center = location
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        Person.center = CGPointMake(160, 330)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
